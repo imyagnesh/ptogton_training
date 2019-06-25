cc58f5e521d01f072e7ceef00bd605b9ecdb6d10
Yagnesh Modh <yagnesh.ash.modh@gmail.com>
2019-06-25 11:09:55 +0530

    Complete eslint + prettier setup

--- AwesomeProject/.eslintrc.js
+++ AwesomeProject/.eslintrc.js
@@ -1,4 +1,39 @@
 module.exports = {
-  root: true,
-  extends: '@react-native-community',
+  env: {
+    browser: true,
+    es6: true,
+    "react-native/react-native": true
+  },
+  parser: "babel-eslint",
+  extends: ["airbnb", "prettier", "plugin:react/recommended", "prettier/react"],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly"
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: [
+    "babel",
+    "import",
+    "jsx-a11y",
+    "prettier",
+    "react",
+    "react-native",
+    "react-hooks"
+  ],
+  rules: {
+    "react/jsx-filename-extension": [
+      2,
+      {
+        extensions: [".js", ".jsx"]
+      }
+    ],
+    "react/forbid-prop-types": [0],
+    "prettier/prettier": ["error"]
+  }
 };
